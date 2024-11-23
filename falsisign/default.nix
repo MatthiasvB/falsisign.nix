0f7579a59743cf641818a35ecb356bc9179b928a
Matthias vom Bruch <matthias.vombruch@gmx.de>
2024-11-23 16:59:27 +0100

    skip tests

--- falsisign/default.nix
+++ falsisign/default.nix
@@ -1,21 +1,26 @@
-{ pkgs, falsisign-src, custom-rotation ? "seq .1 .1 .5" }:
-
-with pkgs;
-
-let
-  makeDerivation = { name, prePostFixup ? "" }:
-    let
-      fixedPath = "${lib.makeBinPath ([ coreutils file ghostscript imagemagick ] ++ lib.optionals (name == "falsisign") [ pdftk ])}";
-    in
+{
+  pkgs,
+  falsisign-src,
+  custom-rotation ? "seq .1 .1 .5",
+}:
+with pkgs; let
+  makeDerivation = {
+    name,
+    prePostFixup ? "",
+  }: let
+    fixedPath = "${lib.makeBinPath ([coreutils file ghostscript imagemagick] ++ lib.optionals (name == "falsisign") [pdftk])}";
+  in
     stdenv.mkDerivation {
       pname = name;
       version = "0.1.0";
 
-      nativeBuildInputs = [ makeWrapper ];
+      nativeBuildInputs = [makeWrapper];
 
-      buildInputs = [ coreutils file ghostscript pdftk imagemagick ];
+      buildInputs = [coreutils file ghostscript pdftk imagemagick];
 
       src = falsisign-src;
+
+      buildPhase = ":";
 
       installPhase = ''
         mkdir -p $out/bin
@@ -23,24 +28,25 @@ let
         chmod +x $out/bin/${name}
       '';
 
-      postFixup = prePostFixup + ''
-        # replace bash
-        substituteInPlace $out/bin/${name} --replace '#!/bin/bash' '#! ${bash}/bin/bash'
+      postFixup =
+        prePostFixup
+        + ''
+          # replace bash
+          substituteInPlace $out/bin/${name} --replace '#!/bin/bash' '#! ${bash}/bin/bash'
 
-        # fix path
-        wrapProgram $out/bin/${name} --prefix PATH : ${fixedPath}
-      '';
+          # fix path
+          wrapProgram $out/bin/${name} --prefix PATH : ${fixedPath}
+        '';
 
       meta = with lib; {
         homepage = "https://gitlab.com/edouardklein/falsisign";
         license = licenses.wtfpl;
         description = "Command-line tool to simulate a document print-sign-and-scan process. Save trees, ink, time, and stick it to the bureaucrats!";
-        maintainers = [ maintainers.gvolpe ];
+        maintainers = [maintainers.gvolpe];
         platforms = platforms.all;
       };
     };
-in
-{
+in {
   falsisign = makeDerivation {
     name = "falsisign";
     prePostFixup = ''
--- flake.nix
+++ flake.nix
@@ -2,34 +2,37 @@
   description = "Falsisign script derivation";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     falsisign-src = {
-      url = gitlab:edouardklein/falsisign;
+      url = "gitlab:edouardklein/falsisign";
       flake = false;
     };
-    flake-utils.url = github:numtide/flake-utils;
+    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
-      url = github:edolstra/flake-compat;
+      url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, falsisign-src, flake-utils, ... }:
-    let
-      forSystem = system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          drv = pkgs.callPackage ./falsisign {
-            inherit pkgs falsisign-src;
-          };
-        in
-		{
-		  packages = {
-            falsisign = drv.falsisign;
-			signdiv = drv.signdiv;
-          };
-          packages.default = drv.falsisign;
-        };
-    in
+  outputs = {
+    self,
+    nixpkgs,
+    falsisign-src,
+    flake-utils,
+    ...
+  }: let
+    forSystem = system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      drv = pkgs.callPackage ./falsisign {
+        inherit pkgs falsisign-src;
+      };
+    in {
+      packages = {
+        falsisign = drv.falsisign;
+        signdiv = drv.signdiv;
+      };
+      packages.default = drv.falsisign;
+    };
+  in
     flake-utils.lib.eachDefaultSystem forSystem;
 }
